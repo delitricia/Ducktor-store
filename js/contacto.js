39a6085397c56660b9ebe54a71e13ebd8bc7b13e
Patricia <delitricia@hotmail.com>
2025-11-26 14:36:36 +0100

    added the form message

--- js/contacto.js
+++ js/contacto.js
@@ -1,1 +1,9 @@
 const formulario = document.getElementById('form_duck');
+
+formulario.addEventListener ("submit", function(event) {
+    event.preventDefault(); 
+    console.log("Mensaje enviado");
+
+    const message = document.getElementById("confirm_message");
+    message.textContent = "¡Gracias por contactarnos!";
+})
