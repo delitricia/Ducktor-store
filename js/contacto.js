c9579281ba0bc4baf6ea68962b7187214e87a747
Patricia <delitricia@hotmail.com>
2025-11-27 12:41:07 +0100

    fixed conflicts with rebase

--- js/contacto.js
+++ js/contacto.js
@@ -4,6 +4,20 @@ formulario.addEventListener ("submit", function(event) {
     event.preventDefault(); 
     console.log("Mensaje enviado");
 
-    const message = document.getElementById("confirm_message");
-    message.textContent = "¡Gracias por contactarnos!";
+    const name = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    const personalData = {
+        name : name,
+        phone : phone,
+        email : email,
+        message : message
+
+    }
+    console.log(personalData);
+
+    const confirmMessage = document.getElementById("confirm_message");
+    confirmMessage.textContent = "¡Gracias por contactarnos!";
 })
